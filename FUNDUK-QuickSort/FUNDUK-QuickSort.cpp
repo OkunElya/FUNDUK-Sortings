b95d0e856778ca296b67afe4556bc5a41d90b91b
OkunElya <okun.elya@gmail.com>
2025-01-23 13:12:00 +1000

    added descending dort

--- FUNDUK-QuickSort/FUNDUK-QuickSort.cpp
+++ FUNDUK-QuickSort/FUNDUK-QuickSort.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 
 void printArr(float arr[], size_t arrSize)
@@ -10,7 +9,7 @@ void printArr(float arr[], size_t arrSize)
 	std::cout << arr[arrSize - 1] << "}";
 }
 
-void quickSort(float arr[], size_t arrSize) {
+void quickSort(float arr[], size_t arrSize, bool isAscending) {
 	if (arrSize < 2) {
 		return;
 	}
@@ -20,10 +19,10 @@ void quickSort(float arr[], size_t arrSize) {
 	float* j = arr + arrSize - 1;
 
 	do{
-		while ((*i <= pivot) && (i < arr + arrSize)) {
+		while ((((*i < pivot)== isAscending)||(*i==pivot)) && (i < arr + arrSize)) {
 			i++;//move until finding arr[i] > pivot
 		}
-		while ((*j >= pivot)&&(j>arr)) {
+		while ((((*j > pivot) == isAscending) || (*i == pivot)) &&(j>arr)) {
 			j--;// -- < -- 
 		}//swap values
 		
@@ -39,39 +38,36 @@ void quickSort(float arr[], size_t arrSize) {
 	arr[0] = *j;
 	*j = pivot;
 
-	quickSort(arr, (j - arr)+1);
-	quickSort(j + 1, arrSize - (j - arr + 1));
+	quickSort(arr, (j - arr)+1,isAscending);
+	quickSort(j + 1, arrSize - (j - arr + 1), isAscending);
 }
 
-void selectionsSort(float arr[], size_t  arrSize) {
+void selectionsSort(float sortedPart[], size_t  arrSize,bool isAscending) {
 	if (arrSize < 2) {
 		return;//already sorted
 	}
-
-	float* sortedPart = arr-1;
-	float* locMinPtr = arr;
+	float* locMinPtr = sortedPart;
 
 	while (arrSize >0) {
 		for (size_t i = 0; i < arrSize; i++) {//find minimal element in the array
-			std::cout << *locMinPtr << "\n";
-			if (sortedPart[i+1] < *locMinPtr) {
-				locMinPtr = sortedPart + (i + 1);
+			if (isAscending ==(sortedPart[i] < *locMinPtr)) {
+				locMinPtr = sortedPart+i ;
 			}
 		}
-		sortedPart++;
+		
 		auto temp = *sortedPart;
 		*sortedPart = *locMinPtr;
 		*locMinPtr = temp;//write values
+
 		locMinPtr = sortedPart + 1;
+		sortedPart++;
 		arrSize--;
 	}
-
 }
 
-
-bool isSorted(float arr[], size_t arrSize, bool ascending) {
+bool isSorted(float arr[], size_t arrSize, bool isAscending) {
 	for (size_t i = 0; i < (arrSize - 1); i++) {
-		if ((ascending != (arr[i] < arr[i + 1])) && (arr[i] != arr[i + 1])) {
+		if ((isAscending == (arr[i] > arr[i + 1])) && (arr[i] != arr[i + 1])) {
 			return false;
 		}
 	}
@@ -80,10 +76,10 @@ bool isSorted(float arr[], size_t arrSize, bool ascending) {
 
 int main()
 {
-	float aboba[] = { 1,4,3,5 };
+	float aboba[] = { 1,4,3,5,8,7,9,0 };
 	size_t abS = sizeof(aboba) / sizeof(aboba[0]);
 
-	selectionsSort(aboba, abS);
+	quickSort(aboba, abS,0);
 	std::cout << "\n final array:";
 	printArr(aboba, abS);
 	std::cout << isSorted(aboba, abS, true);
