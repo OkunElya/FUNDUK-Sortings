0c28a70335edc2923aa6d2e41df86ffa8165566c
OkunElya <okun.elya@gmail.com>
2025-01-23 13:24:57 +1000

    moved to templates

--- FUNDUK-QuickSort/FUNDUK-QuickSort.cpp
+++ FUNDUK-QuickSort/FUNDUK-QuickSort.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <type_traits>//for concepts usage
 
-void printArr(float arr[], size_t arrSize)
+template <typename T>
+concept isComparable = requires(T a, T b) { // a concept to check if type is comparable
+	{ a > b } -> std::same_as<bool>;
+	{ a < b } -> std::same_as<bool>;
+	{ a == b } -> std::same_as<bool>;
+};
+
+template <class T>
+void printArr(T arr[], size_t arrSize)
 {
 	std::cout << "{";
 	for (size_t i = 0; i < (arrSize - 1); i++) {
@@ -9,14 +18,16 @@ void printArr(float arr[], size_t arrSize)
 	std::cout << arr[arrSize - 1] << "}";
 }
 
-void quickSort(float arr[], size_t arrSize, bool isAscending) {
+template <class T>
+void quickSort(T arr[], size_t arrSize, bool isAscending)requires isComparable<T> {
 	if (arrSize < 2) {
 		return;
 	}
 
-	float pivot = arr[0];
-	float* i = arr + 1;
-	float* j = arr + arrSize - 1;
+	T pivot = arr[0];
+	T* i = arr + 1;
+	T* j = arr + arrSize - 1;
+	T* temp = new T;
 
 	do{
 		while ((((*i < pivot)== isAscending)||(*i==pivot)) && (i < arr + arrSize)) {
@@ -27,9 +38,10 @@ void quickSort(float arr[], size_t arrSize, bool isAscending) {
 		}//swap values
 		
 		if (i < j) {
-			float temp = *i;
+			
+			*temp=*i;
 			*i = *j;
-			*j = temp;
+			*j = *temp;
 			i++;
 			j--;
 		}
@@ -37,17 +49,21 @@ void quickSort(float arr[], size_t arrSize, bool isAscending) {
 
 	arr[0] = *j;
 	*j = pivot;
+	temp = nullptr;
+	delete temp;
 
 	quickSort(arr, (j - arr)+1,isAscending);
 	quickSort(j + 1, arrSize - (j - arr + 1), isAscending);
+
 }
 
-void selectionsSort(float sortedPart[], size_t  arrSize,bool isAscending) {
+template <class T>
+void selectionsSort(T sortedPart[], size_t  arrSize,bool isAscending)requires isComparable<T> {
 	if (arrSize < 2) {
 		return;//already sorted
 	}
-	float* locMinPtr = sortedPart;
-
+	T* locMinPtr = sortedPart;
+	T* temp = new T;//using a pointer to not run destructor while copying the object
 	while (arrSize >0) {
 		for (size_t i = 0; i < arrSize; i++) {//find minimal element in the array
 			if (isAscending ==(sortedPart[i] < *locMinPtr)) {
@@ -55,17 +71,19 @@ void selectionsSort(float sortedPart[], size_t  arrSize,bool isAscending) {
 			}
 		}
 		
-		auto temp = *sortedPart;
+		*temp = *sortedPart;
 		*sortedPart = *locMinPtr;
-		*locMinPtr = temp;//write values
-
+		*locMinPtr = *temp;//write values
 		locMinPtr = sortedPart + 1;
 		sortedPart++;
 		arrSize--;
 	}
+	temp = nullptr;
+	delete temp;
 }
 
-bool isSorted(float arr[], size_t arrSize, bool isAscending) {
+template <class T>
+bool isSorted(T arr[], size_t arrSize, bool isAscending)requires isComparable<T> {
 	for (size_t i = 0; i < (arrSize - 1); i++) {
 		if ((isAscending == (arr[i] > arr[i + 1])) && (arr[i] != arr[i + 1])) {
 			return false;
@@ -79,7 +97,7 @@ int main()
 	float aboba[] = { 1,4,3,5,8,7,9,0 };
 	size_t abS = sizeof(aboba) / sizeof(aboba[0]);
 
-	quickSort(aboba, abS,0);
+	selectionsSort(aboba, abS,0);
 	std::cout << "\n final array:";
 	printArr(aboba, abS);
 	std::cout << isSorted(aboba, abS, true);
